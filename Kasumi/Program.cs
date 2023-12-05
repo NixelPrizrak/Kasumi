8ff5ea3e8a163760a30fa313f4b45fb6f4b64abf
nixel <notu.ru@mail.ru>
2023-12-05 04:12:48 +0300

    Добавьте файлы проекта.

---
+++ Kasumi/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kasumi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите строку для зашифровки:");
+                string input = Console.ReadLine();
+                input = KASUMI.Encode(input);
+                Console.WriteLine($"Строка после зашифровки: \n{input}");
+                input = KASUMI.Decode(input);
+                Console.WriteLine($"Строка после дешифровки: \n{input}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
